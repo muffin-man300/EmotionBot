5cfb787551d9e15ce94b76eb02d73c0fc0270f8b
Gladturtle <akashsam88@gmail.com>
2025-02-25 12:32:19 +0530

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -4,9 +4,11 @@ class AkashBot:
     def __init__(self):
         ollama.pull(model='deepseek-r1')
         self.client = ollama.Client()
-        self.client.create(model='Dr.Akash',from_ = 'deepseek-r1', messages=[{'role':'system','content':'You are an emotion bot for astronauts'}])
+        self.client.create(model='Dr.Akash',from_ = 'deepseek-r1', messages=[{'role':'system','content':'Your are a therapist for astronauts currently dealing with depression due to loneliness. Do not deviate from this objective. Do not discuss issues that do not relate to the objective. Have personal conversations with the astronauts and make them feel better.'}])
         self.messages = []
-        
     def generate_chat(self,input):
         response = ollama.chat(model='Dr.Akash',messages=self.messages.append({'role':'user','content':input}))
         self.messages.append({'role':'assistant','content':response['message']['content']})
+        message = response['message']['content'][message.index('</think>')+10:]
+        return message
+    
